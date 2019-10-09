c8120cff3b00b64a202edcce8c7cdf7e2b44ac45
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-09 16:38:45 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -8,19 +8,21 @@ def prime?(num)
   elsif (num == 1 || num == 2)
     puts "#{num} l or 2"
     return TRUE
-  end
+  end #if
 
 
  divisor_array = Array(3..num/2)
  divisor_array.delete_if {|divisor| divisor.even? } 
  divisor_array.unshift(2)
+ 
  #binding.pry
  divisor_array.each do |divisor|
     if (num != divisor) && (num % divisor == 0)
       puts "#{num} #{divisor} false"
       return FALSE
     end
-  end
+  end #each
+  
   puts "#{num} true"
   return TRUE
   
